8c014b2caac8243e8c5e95223ef5d1a2d504678b
Derek Visch <dvisch@autoidm.com>
2021-04-16 01:44:04 +0000

    Round 1 an hour in, nothing works yet

--- target_mssql/target.py
+++ target_mssql/target.py
@@ -1,9 +1,7 @@
 """MSSQL target class."""
-
 from pathlib import Path
 from typing import List
-
-from singer_sdk import Tap, Stream
+from singer_sdk_target import TargetBase
 from singer_sdk.typing import (
     ArrayType,
     BooleanType,
@@ -16,23 +14,7 @@ from singer_sdk.typing import (
     StringType,
 )
 
-# TODO: Import your custom stream types here:
-from target_mssql.streams import (
-    MSSQLStream,
-    UsersStream,
-    GroupsStream,
-)
-
-
-# TODO: Compile a list of custom stream types here
-#       OR rewrite discover_streams() below with your custom logic.
-STREAM_TYPES = [
-    UsersStream,
-    GroupsStream,
-]
-
-# TODO: Update from Tap to Target
-class TargetMSSQL(Tap):
+class TargetMSSQL(TargetBase):
     """MSSQL tap class."""
 
     name = "target-mssql"
@@ -45,12 +27,6 @@ class TargetMSSQL(Tap):
         Property("password", StringType, required=True),
         Property("database", StringType, required=True),
     ).to_dict()
-
-
-    def discover_streams(self) -> List[Stream]:
-        """Return a list of discovered streams."""
-        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
-
 
 # CLI Execution:
 
