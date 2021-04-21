db8175a0ee92d757b122b3f2653867e29388ab81
Derek Visch <dvisch@autoidm.com>
2021-04-21 02:11:09 +0000

    Decided to go with a simpler version of the sdk_target from target-lunch-money by Douwe. Was passing data in, still failing on Schema messages

--- target_mssql/streams.py
+++ target_mssql/streams.py
@@ -1,65 +1,10 @@
 """Stream class for target-mssql."""
 from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
-from singer_sdk.streams import Stream
-from singer_sdk.typing import (
-    ArrayType,
-    BooleanType,
-    DateTimeType,
-    IntegerType,
-    NumberType,
-    ObjectType,
-    PropertiesList,
-    Property,
-    StringType,
-)
+from singer_sdk.stream import Stream
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 class MSSQLStream(Stream):
     """Stream class for MSSQL streams."""
-
-    def get_records(self, partition: Optional[dict] = None) -> Iterable[dict]:
-        """Return a generator of row-type dictionary objects.
-
-        The optional `partition` argument is used to identify a specific slice of the
-        stream if partitioning is required for the stream. Most implementations do not
-        require partitioning and should ignore the `partitions` argument.
-        """
-        # TODO: Write logic to extract data from the upstream source.
-        # rows = mysource.getall()
-        # for row in rows:
-        #     yield row.to_dict()
-        raise NotImplementedError("The method is not yet implemented (TODO)")
-
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
-class UsersStream(MSSQLStream):
-    name = "users"
-
-    primary_keys = ["id"]
-    replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"
-    schema = PropertiesList(
-        Property("name", StringType),
-        Property("id", StringType),
-        Property("age", IntegerType),
-        Property("email", StringType),
-        Property("street", StringType),
-        Property("city", StringType),
-        Property("state", StringType),
-        Property("zip", StringType),
-    ).to_dict()
-
-
-class GroupsStream(MSSQLStream):
-    name = "groups"
-    primary_keys = ["id"]
-    replication_key = "modified"
-    schema = PropertiesList(
-        Property("name", StringType),
-        Property("id", StringType),
-        Property("modified", DateTimeType),
-    ).to_dict()
 
--- target_mssql/target.py
+++ target_mssql/target.py
@@ -1,33 +1,27 @@
 """MSSQL target class."""
 from pathlib import Path
 from typing import List
-from singer_sdk_target import TargetBase
-from singer_sdk.typing import (
-    ArrayType,
-    BooleanType,
-    DateTimeType,
-    IntegerType,
-    NumberType,
-    ObjectType,
-    PropertiesList,
-    Property,
-    StringType,
-)
+from singer_sdk.target import Target
+from .streams import MSSQLStream
+class TargetMSSQL(Target):
+  """MSSQL tap class."""
+  STREAM_TYPES = [
+    MSSQLStream,
+  ]   
+    
+  name = "target-mssql"
 
-class TargetMSSQL(TargetBase):
-    """MSSQL tap class."""
-
-    name = "target-mssql"
-
-    # TODO: Update this section with the actual config values you expect:
-    config_jsonschema = PropertiesList(
-        Property("host", StringType, required=True),
-        Property("user", StringType, required=True),
-        Property("port", IntegerType, required=False, default=1521),
-        Property("password", StringType, required=True),
-        Property("database", StringType, required=True),
-    ).to_dict()
-
+  # TODO: Update this section with the actual config values you expect:
+  #config_jsonschema = PropertiesList(
+  #    Property("host", StringType, required=True),
+  #    Property("user", StringType, required=True),
+  #    Property("port", IntegerType, required=False, default=1521),
+  #    Property("password", StringType, required=True),
+  #    Property("database", StringType, required=True),
+  #).to_dict()
+  def streams(self):
+    return [stream_class(target=self) for stream_class in STREAM_TYPES]
+    
 # CLI Execution:
 
-cli = TargetMSSQL.cli
+cli = TargetMSSQL.cli()
