19a37a7815206d038cc3830fe8228f9ddb4b587d
Derek Visch <dvisch@autoidm.com>
2021-05-11 17:43:27 +0000

    Windows Authentication support!

--- target_mssql/target.py
+++ target_mssql/target.py
@@ -15,19 +15,12 @@ class TargetMSSQL(Target):
   def __init__(self, config, *args, **kwargs):
     super().__init__(config, *args, **kwargs)
     assert self.config["host"]
-    self.conn = pymssql.connect(server=self.config["host"], 
-                                user=self.config["user"],
-                                password=self.config["password"],
-                                database=self.config["database"],
-                                port=self.config["port"])
-  # TODO: Update this section with the actual config values you expect:
-  #config_jsonschema = PropertiesList(
-  #    Property("host", StringType, required=True),
-  #    Property("user", StringType, required=True),
-  #    Property("port", IntegerType, required=False, default=1521),
-  #    Property("password", StringType, required=True),
-  #    Property("database", StringType, required=True),
-  #).to_dict()
+    self.conn = pymssql.connect(host=self.config["host"], 
+                                user=self.config.get("user"),
+                                password=self.config.get("password"),
+                                database=self.config.get("database"),
+                                port=self.config.get("port", 1433) #If port is None, Windows Authentication will fail due to defaults in pymssql. https://github.com/pymssql/pymssql/blob/ff84c14157cf7b1cc528f68d72f07c2d0fb7d290/src/pymssql/_mssql.pyx#L613 
+                                )
 
   #TODO not a fan of streams not being required by the BaseTarget class here, as it's referenced in the class
   def streams(self):
